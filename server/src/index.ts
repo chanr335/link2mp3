2145ba41c8938e4ee193deacfed554eb58609ac9
chanr335 <76451685+chanr335@users.noreply.github.com>
2024-07-20 00:49:48 -0400

    kinde halfway implemented

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,11 +7,11 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
-app.use(router)
-
 app.get('/', (req, res) => {
   res.send('Bomboclat Server');
 });
+
+app.use('/api', router)
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
--- server/src/routes/authRoute.ts
+++ server/src/routes/authRoute.ts
@@ -1,12 +1,17 @@
-import {Request, Response, Router} from 'express';
-const router = Router()
+import { Request, Response, Router } from "express";
 
-router.get('/hallo', (req:Request, res:Response) => {
-    res.send("Get Request")
-})
+const router = Router();
 
-router.post('/test', (req:Request, res:Response) => {
-    res.send("Post Request")
-})
+router.get("/login", (req: Request, res: Response) => {
+  return res.redirect("/");
+});
 
-export default router
+router.get("/register", kindeClient.register, (req: Request, res: Response) => {
+  return res.redirect("/");
+});
+
+router.get("/callout", (req: Request, res: Response) => {
+  return res.redirect("/");
+});
+
+export default router;
